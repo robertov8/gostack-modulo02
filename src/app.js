69e24f61c59324b45272ce1179901da2fc765d2f
Roberto Ribeiro <robertov82008@gmail.com>
2020-02-08 13:07:29 -0300

    21 - Tratamento de exceções

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
 import path from 'path';
+import Youch from 'youch';
+
+import 'express-async-errors';
+
 import express from 'express';
 import routes from './routes';
 
@@ -9,6 +13,7 @@ class App {
     this.server = express();
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
@@ -21,6 +26,14 @@ class App {
 
   routes() {
     this.server.use(routes);
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errros = await new Youch(err, req).toJSON();
+
+      return res.status(500).json(errros);
+    });
   }
 }
 
