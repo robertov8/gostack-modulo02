c048e380ce8627e09e628880ff24f9b88625e673
Roberto Ribeiro <robertov82008@gmail.com>
2020-02-08 11:45:30 -0300

    20 - Campos virtuais no agendamento

--- src/app/controllers/AvailableController.js
+++ src/app/controllers/AvailableController.js
@@ -59,7 +59,7 @@ class AvailableController {
         time,
         value: format(value, "yyyy-MM-dd'T'HH:mm:ssxxx"),
         available:
-          isAfter(value, new Date(2020, 1, 8)) &&
+          isAfter(value, new Date()) &&
           !appointments.find(a => format(a.date, 'HH:mm') === time),
       };
     });
